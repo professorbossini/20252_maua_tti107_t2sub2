c556b6bb144a84a880bf4be5185aaa7af1c958c4
Rodrigo Bossini <professorbossini@gmail.com>
2025-10-06 12:44:21 -0300

    ap08-js: pg 2
    - vimos var
    - retomar falando de tipos, comparacao e coercao

---
+++ 07_javascript_intro/index.js
@@ -0,0 +1,30 @@
+//declaração de variáveis e constantes
+//var (a gente prefere não usar)
+var idade = 18
+console.log('Olá, ' + nome)
+if(idade >= 18){
+  let nome = 'João'
+  console.log(`Parabéns, ${nome}. Você pode dirigir.`)
+}
+else{
+  console.log('Infelizmente ainda não.')
+}
+console.log(`Até mais, ${nome}.`)
+// console.log(idade)
+// idade = idade + 1
+// console.log(idade)
+
+// let a = 2
+// let nome = 'Maria'
+// console.log(a)
+// a = 5
+// console.log(a)
+
+// const nome = 'José'
+// const idade = 27
+// console.log(nome)
+// nome = 'José Silva'
+// console.nome(nome)
+
+// console.log("Hello, World!")
+// console.log("Estamos usando o NodeJS")
