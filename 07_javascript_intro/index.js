75ceee9c4d0566fc1f1da529ceb075cb0e4e717a
Rodrigo Bossini <professorbossini@gmail.com>
2025-10-10 09:09:19 -0300

    ap08-js: encerra capitulo 1
    - retomaremos na apostila 09, fazendo o cadastro de filmes full stack

--- 07_javascript_intro/index.js
+++ 07_javascript_intro/index.js
@@ -1,15 +1,92 @@
+// //funções regulares
+// //arrow functions
+// const triplo = (n) => { 
+//   console.log(n)
+//   return n * 3
+//   // return undefined 
+// }
+// console.log(triplo(10))
+// const dobro = (n = 10) => n * 2
+// console.log(dobro(undefined))
+// console.log(dobro(20))
+
+// const hello = () =>  console.log('oi') 
+// hello()
+// const triplo = function(n = 5){
+//   return n * 3
+// }
+// console.log(triplo(10))
+// console.log(triplo(undefined))
+// const dobro = function (n){
+//   return 2 * n
+// }
+// console.log(dobro(5))
+// function hello(nome){
+//   console.log(`Oi, ${nome}. Tudo bem?`)
+// }
+// hello('Ana')
+// hello(undefined)
+
+// function somar(a, b){
+//   return a + b
+// }
+// const res = somar(2, 3)
+// console.log(res)
+// hello('Ana')
+// function hello(){
+//   console.log('oi')
+// }
+
+// hello()
+// hello()
+
+//declaração de vetor/coleção/lista/array
+// v1 = []
+// v1[0] = 3.4
+// console.log(v1)
+// console.log(v1.length)
+// v1[10] = "abc"
+// console.log(v1)
+// console.log(v1.length)
+//operador de comparação no js
+// == ou ===
+//null e undefined
+// console.log([] == [])
+// console.log([0] == false)
+// console.log(null == undefined)
+// console.log(undefined == undefined)
+// console.log(null == null)
+// let a = null
+// let b = undefined
+// console.log([1] == 1)
+// console.log(1 == true)
+// console.log(1 === "1")
+// console.log(1 === 1)
+// console.log(1 == "1")
+// console.log(1 == 1)
+
+//coerção
+// const n1 = 2 //number
+// const n2 = '3' //string
+// //coerção implícita
+// const n3 = n1 + n2
+// console.log(n3)
+// //coerção explícita
+// const n4 = n1 + Number(n2)
+// console.log(n4)
+
 //declaração de variáveis e constantes
 //var (a gente prefere não usar)
-var idade = 18
-console.log('Olá, ' + nome)
-if(idade >= 18){
-  let nome = 'João'
-  console.log(`Parabéns, ${nome}. Você pode dirigir.`)
-}
-else{
-  console.log('Infelizmente ainda não.')
-}
-console.log(`Até mais, ${nome}.`)
+// var idade = 18
+// console.log('Olá, ' + nome)
+// if(idade >= 18){
+//   let nome = 'João'
+//   console.log(`Parabéns, ${nome}. Você pode dirigir.`)
+// }
+// else{
+//   console.log('Infelizmente ainda não.')
+// }
+// console.log(`Até mais, ${nome}.`)
 // console.log(idade)
 // idade = idade + 1
 // console.log(idade)
