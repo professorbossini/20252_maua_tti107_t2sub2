d1fdd0576b77837a64ab7ca3428d4a19465f620b
Rodrigo Bossini <professorbossini@gmail.com>
2025-10-17 09:10:22 -0300

    ap09-fullstack: encerra pg 9
    - fizemos endpoints de cadastro e listagem
    - retomar comecando o front

--- 08_solucao_full_stack_filmes/back/index.js
+++ 08_solucao_full_stack_filmes/back/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 //aplicando uma função middleware
 app.use(express.json())
+const filmes = [
+  {
+    titulo: 'Interestelar',
+    sinopse: 'Viagem no espaço com salvamento da humidade.'
+  }
+]
 
 app.get('/hey', (req, res) => {
   console.log('Endpoint acionado(GET)...')
@@ -15,6 +21,33 @@ app.post('/oi', (req, res) => {
   console.log('Endpoint acionado(POST)....')
   res.send('oi, tudo bem?')
 })
+
+
+app.get('/filmes', (req, res) => {
+  res.json(filmes)  
+})
+
+/*
+req = {
+  body:{
+    titulo: 'Piratas do Caribe',
+    sinopse: 'Aventura de piratas'
+  }
+}
+*/
+app.post('/filmes', (req, res) => {
+  const titulo = req.body.titulo
+  const sinopse = req.body.sinopse
+  const filme = {
+    titulo: titulo,
+    sinopse: sinopse
+  }
+  filmes.push(filme)
+  res.json(filmes)  
+})
+
+
+
 
 
 
